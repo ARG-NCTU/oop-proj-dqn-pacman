6be4f96d22dcf29506216b306528f1d06fed78b1
hchengwang <hchengwang@gmail.com>
2023-11-13 02:17:45 +0800

    add conv1 i/o pytest

--- tests/test_dqn_model.py
+++ tests/test_dqn_model.py
@@ -1,3 +1,5 @@
+import torch
+
 import pytest
 
 import add_path
@@ -31,4 +33,17 @@ def test_dqn_model_named_params():
         print(name, param.size())
         assert name is not None
         assert param.requires_grad
+
+# check for model conv1 layer inputs/outputs
+def test_dqn_model_conv1():
+
+    policy = DQN(4)
+    conv1_layer = policy.conv1
+    
+    # create an input tensor of size torch.Size([1, 4, 44, 40])
+    input_tensor = torch.randn(1, 4, 44, 40)
+    # feed input tensor to conv1 layer
+    output_tensor = conv1_layer(input_tensor)
+    # check output tensor size
+    assert output_tensor.size() == torch.Size([1, 32, 23, 21])
 
