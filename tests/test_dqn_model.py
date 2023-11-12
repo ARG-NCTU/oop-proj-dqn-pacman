3e63488fea7328277447afc8499a263a456e3366
hchengwang <hchengwang@gmail.com>
2023-11-13 01:20:24 +0800

    add pytest and plot

--- tests/test_dqn_model.py
+++ tests/test_dqn_model.py
@@ -14,6 +14,7 @@ def test_dqn_model():
 def test_dqn_model_modules():
     policy = DQN(4)
     for module in policy.modules():
+        print(module)
         assert module is not None
 
 # check for model parameters
@@ -26,23 +27,8 @@ def test_dqn_model_params():
 def test_dqn_model_named_params():
     policy = DQN(4)
     for name, param in policy.named_parameters():
+        # print name and size of param
+        print(name, param.size())
         assert name is not None
         assert param.requires_grad
-
-# check for nn.optim.Adam optimizer
-def test_dqn_model_optimizer():
-    policy = DQN(4)
-    assert policy.optimizer is not None
-    assert type(policy.optimizer).__name__ == 'Adam'
-
-# check for nn.MSELoss criterion
-def test_dqn_model_criterion():
-    policy = DQN(4)
-    assert policy.criterion is not None
-    assert type(policy.criterion).__name__ == 'MSELoss'
-
-# check for forward pass
-def test_dqn_model_forward():
-    policy = DQN(4)
-    assert policy.forward is not None
 
